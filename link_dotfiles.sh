a3d7668af0dd401f2877ef180af2b7a480331c20
Knut Flock <ksflock@yahoo.com>
2015-08-16 23:30:22 +0000

    Obviously there were some funny characters in there (produced by geany)

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -10,14 +10,14 @@ files="vimrc octaverc screenrc"    # list of files/folders to symlink in homedot
 
 
 # create dotfiles_old in homedotfiles
-if [ ! -d $old_dotfiles ]; then
+if [ ! -d $old_dotfiles ]; then
 	echo "Creating $old_dotfiles for backup of any existing dotfiles in ~"
 	mkdir -p $old_dotfiles
 	echo "...done"
 fi
 
 # change to the dotfiles dotfilesectory
-if [ ! -d $dotfiles ]; then
+if [ ! -d $dotfiles ]; then
 	mkdir -p $dotfiles
 fi
 echo "Changing to the $dotfiles dotfilesectory"
@@ -27,7 +27,7 @@ echo "...done"
 # move any existing dotfiles in homedotfiles to dotfiles_old dotfilesectory, then create symlinks 
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $old_dotfiles"
-    mv ~/.$file ~/dotfiles_old/
+    [ -f ~/.$file ] && mv -f ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home dotfilesectory."
     ln -s $dotfiles/$file ~/.$file
 done
